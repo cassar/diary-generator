e34e071170a1e4421f47db2e73b216f5f592bc36
Luke Cassar <luke.cassar@uqconnect.edu.au>
2019-09-18 13:42:48 +1000

    Initial Generator script

---
+++ generator.rb
@@ -0,0 +1,19 @@
+require 'erb'
+require 'active_support/all'
+require 'pry'
+
+# TODO: Move to Repo
+
+PATH = "/Users/lukecassar/Google\ Drive/Personal\ Organisation\ Stuff/Diary/2019"
+TOMORROW = 'tomorrow'
+
+time = Time.now
+time = time.tomorrow if ARGV.first == TOMORROW
+ordinal_day = time.day.ordinalize
+month = time.month
+month_name = time.strftime '%B'
+year = time.year
+output = ERB.new(File.read(PATH + '/typical_work_day_template.md')).result
+directory_name = "#{0 if month < 10}#{month}_#{month_name.downcase}"
+Dir.mkdir(directory_name) unless File.exists? directory_name
+File.write "#{PATH}/#{directory_name}/#{ordinal_day}.md", output
