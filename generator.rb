8634499765099bca2cdd53f9850e4c4c5aac5e6b
Luke Cassar <luke.cassar@uqconnect.edu.au>
2019-10-01 13:17:20 +1000

    Classify Generator

--- generator.rb
+++ generator.rb
@@ -2,24 +2,54 @@ require 'erb'
 require 'active_support/all'
 require 'pry'
 
-DIARY_PATH = File.read('.diary_config').strip
-TOMORROW = 'tomorrow'
+class DiaryGenerator
+  DIARY_PATH = File.read('.diary_config').strip
+  TOMORROW = 'tomorrow'
 
-def template_path(time)
-  if ['Saturday', 'Sunday'].include? time.strftime("%A")
-    '/typical_day_template.md'
-  else
-    '/typical_work_day_template.md'
+  attr_reader :time, :ordinal_day, :month, :month_name, :year, :directory_path
+
+  def initialize
+    @time = determine_time
+    @ordinal_day = time.day.ordinalize
+    @month = time.month
+    @month_name = time.strftime '%B'
+    @year = time.year
+    @directory_path = determine_path
+  end
+
+  def heading
+    "#{ordinal_day} #{month_name} #{year}"
+  end
+
+  def generate!
+    Dir.mkdir directory_path unless File.exists? directory_path
+    File.write "#{directory_path}/#{ordinal_day}.md", content
+  end
+
+  private
+
+  def template_path(time)
+    if ['Saturday', 'Sunday'].include? time.strftime("%A")
+      '/typical_day_template.md'
+    else
+      '/typical_work_day_template.md'
+    end
+  end
+
+  def determine_time
+    return Time.now.tomorrow if ARGV.first == TOMORROW
+
+    Time.now
+  end
+
+  def content
+    ERB.new(File.read(DIARY_PATH + template_path(time))).result
+  end
+
+  def determine_path
+    "#{DIARY_PATH}/#{0 if month < 10}#{month}_#{month_name.downcase}"
   end
 end
 
-time = Time.now
-time = time.tomorrow if ARGV.first == TOMORROW
-ordinal_day = time.day.ordinalize
-month = time.month
-month_name = time.strftime '%B'
-year = time.year
-output = ERB.new(File.read(DIARY_PATH + template_path(time))).result
-directory_path = "#{DIARY_PATH}/#{0 if month < 10}#{month}_#{month_name.downcase}"
-Dir.mkdir(directory_path) unless File.exists? directory_path
-File.write "#{directory_path}/#{ordinal_day}.md", output
+generator = DiaryGenerator.new
+generator.generate!
